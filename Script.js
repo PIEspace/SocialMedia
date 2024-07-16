a82ef6b84018f4bfb992e71d15a9424576eb9119
PROJECTOCCUPYMARS <134577378+PIEspace@users.noreply.github.com>
2024-07-17 03:17:19 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('a');
-    links.forEach(link => {
-        link.addEventListener('click', (event) => {
-            alert(`You are now leaving to visit ${event.target.textContent}`);
-        });
-    });
-});
